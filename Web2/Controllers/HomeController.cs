b3462fa1df176d93825f6a6b7c4859fbac8feccb
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2022-08-16 10:08:59 +0300

    2th completed

---
+++ Web2/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Web2.Models;
+
+namespace TestMvcApp
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new ViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(ViewModel model)
+        {
+            var service = new PaymentService(new InputParams { CountPeople = model.PlayersNumber, InitAmount = model.StartAmount });
+            var result = service.Start().Result;
+            ViewBag.Result = result;
+            return View(new ViewModel());
+        }
+    }
+}
---
+++ Web2/Models/ViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Web2.Models
+{
+    public class ViewModel
+    {
+        public decimal StartAmount { get; set; }
+        public int PlayersNumber { get; set; }
+    }
+
+    public class InputParams
+    {
+
+        public decimal InitAmount { get; set; }
+        public decimal BalanceAmount { get; set; }
+        public int CountPeople { get; set; }
+
+    }
+
+    public class PaymentService
+    {
+        private InputParams inputParams;
+        private CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+        private CancellationToken token;
+        private object lockObj = new object();
+
+
+        public PaymentService(InputParams inputParams)
+        {
+            this.inputParams = inputParams;
+            token = cancelTokenSource.Token;
+        }
+
+        public async Task<InputParams> Start()
+        {
+            this.inputParams.BalanceAmount = this.inputParams.InitAmount;
+            List<Task> players = new List<Task>();
+            for (var i = 0; i < this.inputParams.CountPeople; i++)
+            {
+                Task task = new Task(TakeAmount, token);
+                players.Add(task);
+            }
+            players.ForEach(x => x.Start());
+
+            Task.WaitAny(players.ToArray());
+            return this.inputParams;
+        }
+
+        private void TakeAmount()
+        {
+            while (true)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                lock (lockObj)
+                {
+                    if (CanTakeMore())
+                    {
+                        //Delay for data base trunsaction
+                        Task.Delay(100);
+                        this.inputParams.BalanceAmount = this.inputParams.BalanceAmount - 0.1M;
+                    }
+                    else
+                    {
+                        cancelTokenSource.Cancel();
+                        return;
+                    }
+                }
+            }
+        }
+
+        private bool CanTakeMore()
+        {
+            if (this.inputParams.BalanceAmount > 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
